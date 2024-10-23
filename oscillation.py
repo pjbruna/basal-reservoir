53995741d3ee9a9e5a537b55ced035528af89afe
Polyphony J. Bruna <50676634+pjbruna@users.noreply.github.com>
2024-10-23 09:31:41 -0700

    Update oscillation.py

--- oscillation.py
+++ oscillation.py
@@ -11,19 +11,19 @@ train_labels = ['1', '2'] * 1000
 
 # Train model
 
-model = ResonanceNetwork(input_nnodes=len(train_data[0]), nnodes=100, p_link=0.1, leak=0.75, lrate_targ=0.01, lrate_wmat=0.1, targ_min=1)
+model = ResonanceNetwork(input_nnodes=len(train_data[0]), nnodes=10, p_link=0.1, leak=0.75, lrate_targ=0.01, lrate_wmat=0.1, targ_min=1)
 results = model.run(train_data)
 
 # Cue trained model
 
-m = 1 # select matrix for correlations: 0 - spikes, 1 - activations, 2 - reservoir weights
+state = 0 # select matrix for correlations: 0 - spikes, 1 - activations, 2 - reservoir weights
 
 cue = [[0,0], [0,0], [0,0], [0,0]]
 cue_labels = ['NA', 'NA', 'NA', 'NA']
 
 cue_results = model.echo(cue)
 
-print(cue_results[m].sum(axis=0)) # fading memory
+print(cue_results[state].sum(axis=0)) # fading memory
 
-cue1_cor_matrix = correlate_states(results[m], cue_results[m])
+cue1_cor_matrix = correlate_states(results[state], cue_results[state])
 plot_correlations(cue1_cor_matrix, train_labels, cue_labels)
